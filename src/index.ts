405b91ea069bfd22f0260b5e1ddb44e3befeaaf7
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-10-01 19:32:06 +0200

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ type Task = {
 const list =  document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("#new-task-form") as HTMLFormElement
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = [ ]
 
 form?.addEventListener("submit", e => {
   e.preventDefault()
@@ -23,15 +24,21 @@ form?.addEventListener("submit", e => {
     completed: false,
     createdAt: new Date(),
   }
+  tasks.push(newTask)
+  tasks.push(true)
   
   addListItem(newTask)
-  input.value
+  input.value = " "
 })
 
 function addListItem (task: Task) {
   const item = document.createElement("li")
   const label = document.createElement("label")
   const checkbox = document.createElement("input")
+  checkbox.addEventListener ("change",() => {
+    task.completed = checkbox.checked
+    console.log(tasks)
+  })
   checkbox.type = "checkbox"
   checkbox.checked = task.completed
   label.append(checkbox, task.title)
