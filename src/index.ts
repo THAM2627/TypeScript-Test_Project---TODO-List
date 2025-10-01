861642e6699437616a21f72794255188acfba8a0
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-10-01 15:54:26 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 const list =  document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("#new-task-form") as HTMLFormElement
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if(input?.value == "" || input?.value==null) return
+
+  const task = {
+    id:
+    title:
+    completed: false,
+    createdAt: new Date,
+  }
+  input.value
+})
