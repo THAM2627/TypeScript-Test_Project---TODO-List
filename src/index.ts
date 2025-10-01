778893ee8eee36b5e5cee67c1d7a711e0c9159f5
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-10-01 18:43:34 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { v4 as uuidV4 } from 'uuid'
 
+type Task = {
+   id: string,
+  title: string,
+  completed: Boolean,
+  createdAt: Date,
+  
+}
+
 const list =  document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("#new-task-form") as HTMLFormElement
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
@@ -12,8 +20,13 @@ form.addEventListener("submit", e => {
   const task = {
     id: uuidV4(),
     title: input.value,
-    completed: false,
-    createdAt: new Date()
+    completed: Boolean,
+    createdAt: Date(),
   }
-  input.value
+  
+  addListItem(newTask)
 })
+
+function addListItem (task: Task) {
+
+}
